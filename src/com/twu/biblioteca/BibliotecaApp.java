3070946d4f5376d1c64862310156f2922c08d3eb
Shelton Suen <sheltonsuen@gmail.com>
2019-04-13 10:56:58 +0800

    [1.2] - display a list of all library books after the welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,19 @@
 package com.twu.biblioteca;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BibliotecaApp {
+
+    private final static List<String> BOOK_LIST = Arrays.asList(
+            "Romance of the Three Kingdoms",
+            "Journey to the West",
+            "A Dream in Red Mansions",
+            "Water Margin");
 
     public static void main(String[] args) {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Biblioteca!");
+
+        BOOK_LIST.forEach(System.out::println);
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -24,9 +24,25 @@ public class BibliotecaAppTest {
     public void should_see_welcome_message_when_start_the_application() {
         BibliotecaApp.main(new String[0]);
 
+
+        String actualResult = mockOutputStream.toString().split("\n")[0];
         assertEquals(
                 "welcome message",
-                "Welcome to Biblioteca. Your one-stop-shop for great book titles in Biblioteca!\n",
+                "Welcome to Biblioteca. Your one-stop-shop for great book titles in Biblioteca!",
+                actualResult);
+    }
+
+    @Test
+    public void should_see_all_books_after_welcome_message() {
+        BibliotecaApp.main(new String[0]);
+
+        assertEquals(
+                "book list followed after welcome message",
+                "Welcome to Biblioteca. Your one-stop-shop for great book titles in Biblioteca!\n" +
+                        "Romance of the Three Kingdoms\n" +
+                        "Journey to the West\n" +
+                        "A Dream in Red Mansions\n" +
+                        "Water Margin\n",
                 mockOutputStream.toString());
     }
 
