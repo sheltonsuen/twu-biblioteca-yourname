741d3922173b6a337b4690e0dad5daa9c086f30a
Shelton Suen <sheltonsuen@gmail.com>
2019-04-13 10:37:48 +0800

    [1.1] - display the welcome message when start the application

---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,37 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class BibliotecaAppTest {
+
+    private final ByteArrayOutputStream mockOutputStream = new ByteArrayOutputStream();
+
+    private final PrintStream originalOutputStream = System.out;
+
+    @Before
+    public void setup() {
+        System.setOut(new PrintStream(mockOutputStream));
+    }
+
+    @Test
+    public void should_see_welcome_message_when_start_the_application() {
+        BibliotecaApp.main(new String[0]);
+
+        assertEquals(
+                "welcome message",
+                "Welcome to Biblioteca. Your one-stop-shop for great book titles in Biblioteca!\n",
+                mockOutputStream.toString());
+    }
+
+    @After
+    public void restore() {
+        System.setOut(originalOutputStream);
+    }
+}
